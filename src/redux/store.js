62f539ee7d2c7d7c9601a6e34da54d9b3c049de8
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-21 22:42:18 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,7 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
+
 import contactsReducer from './contacts/contacts';
 import { filterSlice } from './contacts/filter';
-import storage from 'redux-persist/lib/storage';
 
 // const rootReducer = combineReducers({
 //   contacts: contactsReducer,
