0ae948e059a5e3454901edd5121d7a358018f2d0
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-21 23:06:17 +0300

    Update List.jsx

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -5,19 +5,24 @@ import css from './List.module.css';
 import { ReactComponent as DeleteIcon } from '../../assets/close.svg';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { handleRemoveContact } from '../../redux/contacts/item';
+// import { handleRemoveContact } from '../../redux/contacts/item';
+import { deleteContact } from 'redux/contacts/operations';
 
 export const ContactList = () => {
-  const contacts = useSelector(state => state.contacts.items);
-  const filter = useSelector(state => state.contacts.filter);
+  // const contacts = useSelector(state => state.contacts.items);
+  // const filter = useSelector(state => state.contacts.filter);
 
-  const isVisibleContacts = () =>
-    contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  const filterContact = isVisibleContacts();
+  // const isVisibleContacts = () =>
+  //   contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter.toLowerCase())
+  //   );
+  // const filterContact = isVisibleContacts();
+  // const dispatch = useDispatch();
   const dispatch = useDispatch();
 
+  function removeContact(id) {
+    dispatch(deleteContact(id));
+  }
   return (
     <ul className={css.list}>
       {filterContact.map(({ id, name, number }) => {
@@ -27,7 +32,7 @@ export const ContactList = () => {
             <button
               className={css.btn__icon}
               aria-label="Delete contact"
-              onClick={() => dispatch(handleRemoveContact(id))}
+              onClick={() => removeContact(id)}
             >
               <DeleteIcon width="10" heigth="10" />
             </button>
