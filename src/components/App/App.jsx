55ea24276b6f23574e11822cc5f474b793b6371f
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-24 19:58:03 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,8 +2,6 @@ import { ContactForm } from '../Form/Form';
 import { ContactList } from '../List/List';
 import { Filter } from '../Filter/Filter';
 import css from './App.module.css';
-// import { useState } from 'react';
-// import useLocalStorage from '../../hooks/useLocalStorage';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import {
@@ -14,42 +12,12 @@ import {
 import { fetchContacts } from '../../redux/contacts/operations';
 import { toast } from 'react-toastify';
 
-export default function App() {
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
-
-  // const [contacts, setContacts] = useLocalStorage('contacts', []);
-  // const [filter, setFilter] = useState('');
+function App() {
   const contactsItems = useSelector(selectContactsItems);
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
 
   const dispatch = useDispatch();
-
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedСontacts = JSON.parse(contacts);
-
-  //   if (parsedСontacts) {
-  //     this.setState({ contacts: parsedСontacts });
-  //   }
-  // }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   const nextContacts = this.state.contacts;
-  //   const prevContacts = prevState.contacts;
-
-  //   if (nextContacts !== prevContacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(nextContacts));
-  //   }
-  // }
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -65,42 +33,6 @@ export default function App() {
     }
   }, [error]);
 
-  // const formSubmitHandler = formData => {
-  //   addToContacts(formData);
-  // };
-
-  // const addToContacts = contact => {
-  //   const normalizedName = contact.name.toLowerCase();
-  //   const isExist = contacts.some(
-  //     ({ name }) => name.toLowerCase() === normalizedName
-  //   );
-
-  //   if (isExist) {
-  //     return alert(`${contact.name} is already in contacts`);
-  //   }
-
-  //   const contactsList = contacts.concat(contact);
-  //   return setContacts(contactsList);
-  // };
-
-  // const changeFilter = e => {
-  //   setFilter(e.currentTarget.value);
-  // };
-
-  // const getVisibleContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
-
-  // const deleteContact = contactId => {
-  //   setContacts(contacts.filter(contact => contact.id !== contactId));
-  // };
-
-  // const visibleContacts = getVisibleContacts();
-
   return (
     <div className={css.container}>
       {isLoading}
@@ -110,14 +42,16 @@ export default function App() {
       </section>
       <section className={css.section}>
         <h2 className={css.title}>Contacts</h2>
-        {contactsItems.length !==
-        (
+        {contactsItems.length !== 0 ? (
           <>
             <Filter />
             <ContactList />
           </>
+        ) : (
+          toast.error('There are some problems! Try again later.')
         )}
       </section>
     </div>
   );
 }
+export default App;
